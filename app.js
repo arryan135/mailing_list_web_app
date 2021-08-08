668e6b5c3f4f2a795c962491785544af74b3c815
arryan <arryan@umich.edu>
2021-08-08 17:54:56 -0400

    try out faker's features

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const faker = require("faker");
 
+const generateAddress = () => {
+  console.log(faker.address.streetAddress());
+  console.log(faker.address.city());
+  console.log(faker.address.state());
+};
+
+generateAddress();
