aaa074936c9511ee6526251004ceb6b6f0bc7f46
arryan <arryan@umich.edu>
2021-08-08 19:25:57 -0400

    connect mysql to nodeJS

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const faker = require("faker");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "passw00rd",
+  database: "join_us"
+});
+
+const sql_query = "SELECT CURTIME() AS time, CURDATE() AS date, NOW() as now";
+
+connection.query(sql_query, (error, results, fields) => {
+  if (error){
+    throw error;
+  }
+  console.log(results[0].time);
+  console.log(results[0].date.toString());
+  console.log(results[0].now.toString());
+});
+
+connection.end();
+
 
 const generateAddress = () => {
   console.log(faker.address.streetAddress());
@@ -6,4 +28,3 @@ const generateAddress = () => {
   console.log(faker.address.state());
 };
 
-generateAddress();
