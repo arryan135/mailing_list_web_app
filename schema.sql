946708349e6d833723b73f4199e0ff164c60a512
arryan <arryan@umich.edu>
2021-08-08 19:34:22 -0400

    write sql script to insert two users into table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,7 @@ CREATE TABLE users (
   email VARCHAR(255) PRIMARY KEY,
   created_at TIMESTAMP DEFAULT NOW()
 );
+
+INSERT INTO users (email) VALUES 
+("Katie135@yahoo.com"),
+("SamJehings@gmail.com");
