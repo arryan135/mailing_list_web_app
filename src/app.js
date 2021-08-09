2a667c358262cfdea03c186f58554793c2e60b8d
arryan <arryan@umich.edu>
2021-08-08 21:53:41 -0400

    connect express with mysql

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "passw00rd",
+  database: "join_us"
+});
+
 app.get("/", (req, res) => {
-  res.send("You've reached The Home Page");
+  const query = 'SELECT COUNT(*) AS count FROM users'
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    var count = results[0].count;
+    res.send(`We have ${count} users in our db`);
+  });
 });
 
 app.listen(3000, () => {
